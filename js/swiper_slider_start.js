56aff42ceb3bd221f5e8fdbc3fd785279dbd137e
S21dr <68563970+S21dr@users.noreply.github.com>
2020-07-20 23:00:37 +0300

    Add files via upload

---
+++ js/swiper_slider_start.js
@@ -0,0 +1,13 @@
+    var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical',
+      // pagination: {
+      //   el: '.swiper-pagination',
+      //   clickable: true,
+      // },
+      centeredSlides:true,
+      initialSlide:1,
+      loop:true,
+      slideToClickedSlide:true,
+      slidesPerView:3,
+      spaceBetween:50,
+    });
