ae417eb550e713a199d176d16898eb3cb73164fa
S21dr <68563970+S21dr@users.noreply.github.com>
2021-05-13 15:32:55 +0300

    add fetch

--- js/ibg.js
+++ js/ibg.js
@@ -6,6 +6,16 @@ if(ibg[i].querySelector('img')){
 ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
 }
 }
+  const url = 'https://gfs.gov.ru/poisk/?q=%D0%A0%D0%96%D0%94&s=+'
+  
+
+ fetch(url)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 ibg();
